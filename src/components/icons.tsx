3f1c9ef9b2567e30e1765ce5fb367d88a6ed18b4
Jef <incutonez@hotmail.com>
2025-07-12 10:23:04 -0400

    feat: Adding Minor Fit and Finish (#5)
    - Stubbing out Snakes and Ladders route
    - Adding Current Turn to the Tic Tac Toe board
    - Adding confetti and restart counter instead of dialog that pops up

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,3 +1,5 @@
 ﻿export { default as IconTicTacToe } from "@material-symbols/svg-400/outlined/grid_3x3.svg?react";
 
 export { default as IconLoading } from "@material-symbols/svg-400/outlined/hourglass.svg?react";
+
+export { default as IconSnakesAndLadders } from "@material-symbols/svg-400/outlined/tools_ladder.svg?react";
---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+﻿import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({
+	component: RouteComponent,
+});
+
+function RouteComponent() {
+	return (
+		<article>
+			Home
+		</article>
+	);
+}
